54641132c3dbcea94cb0d7ddc0d42bf6ea071cdc
Hyuno Choi <soonitoon@gmail.com>
2022-12-08 23:01:40 +0900

    feat: Make basic structure of handing static files of server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 import express from "express";
 
+const PORT_NUMBER = 3000;
+
 const app = express();
 
-console.log("Hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-app.listen(3000);
+app.get("/", (req, res) => res.render("home"));
+app.use("/public", express.static(__dirname + "/public"));
+
+const handleListen = () =>
+  console.log(`Listening on http://localhost:${PORT_NUMBER}`);
+
+app.listen(3000, handleListen);
