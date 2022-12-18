7369dfa56619f06b7c9ec7f6ca0c763705301bd2
Hyuno Choi <soonitoon@gmail.com>
2022-12-18 17:15:45 +0900

    feat: Make default chat feature

---
+++ src/public/js/app.js
@@ -0,0 +1,27 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("Just got this:", message.data, "from the server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("Hello from the browser!");
+}, 10 * 1000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const PORT_NUMBER = 3000;
@@ -6,11 +8,27 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-
 app.get("/", (req, res) => res.render("home"));
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () =>
   console.log(`Listening on http://localhost:${PORT_NUMBER}`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => {
+    console.log("Disconnected from client ❌");
+  });
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
+});
+
+server.listen(PORT_NUMBER, handleListen);
